7af709d78b03eb26b9913d1923526faa404ad681
Dmitry Sedykh <sedykh@gmail.com>
2021-06-01 04:14:59 +0300

    skip empty html text nodes

--- html.go
+++ html.go
@@ -1,12 +1,15 @@
 package wstat
 
 import (
+	"bytes"
 	"errors"
 	"io"
 
 	"golang.org/x/net/html"
 )
 
+// IgnoreHTMLTags contains the list of names of the HTML tags, the contents of
+// which are ignored.
 var IgnoreHTMLTags = map[string]struct{}{
 	"script": {},
 	"style":  {},
@@ -14,6 +17,8 @@ var IgnoreHTMLTags = map[string]struct{}{
 	"title":  {},
 }
 
+// FromHTML extracts text from HTML and returns statistical information on text.
+// The contents of the tag from the IgnoreHTMLTAGS list is ignored.
 func FromHTML(r io.Reader) (c Counter, err error) {
 	var ignoreDepth int
 	z := html.NewTokenizer(r)
@@ -28,8 +33,13 @@ func FromHTML(r io.Reader) (c Counter, err error) {
 			return
 
 		case html.TextToken:
-			if ignoreDepth == 0 {
-				_, _ = c.Write(z.Text())
+			if ignoreDepth > 0 {
+				continue
+			}
+
+			text := z.Text()
+			if len(bytes.TrimSpace(text)) > 0 {
+				_, _ = c.Write(text) // only not empty text
 			}
 
 		case html.StartTagToken, html.EndTagToken:
